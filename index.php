9d99dce62feda24a26f79feb9ee7d4f7952e1216
ingvild <s236763@oslomet.no>
2020-01-16 13:45:02 +0100

    hade!

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ include_once 'View/minNavigasjon.php';
     <br/><br/>
     <h4>Velkommen til TestingBank</h4>
     <p>Trykk her for å logge inn</p>
-    <h2>Hei Ingvild!!</h2>
     <br/>
     <a href="View/loggInn.php" class="btn btn-success">Logg inn</a>
     <br/> <br/>
