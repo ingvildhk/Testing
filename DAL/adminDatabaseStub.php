c84d06d4f4c9ce2aa6e004c08ca77c4fb1612fa8
ingvild <s236763@oslomet.no>
2020-01-23 12:05:01 +0100

    la til funksjoner i stubs + et nytt dokument
    Har skrevet opp hvilke funksjoner som må lages i stubs,  + endret navn på en som var feil, og flyttet en metode fra bankStub til adminStub som lå feil

--- DAL/adminDatabaseStub.php
+++ DAL/adminDatabaseStub.php
@@ -1,1 +1,57 @@
 <?php
+    include_once '../Model/domeneModell.php';
+    class AdminDBStub
+    {
+        function hentAlleKunder()
+        {
+           $alleKunder = array();
+           $kunde1 = new kunde();
+           $kunde1->personnummer ="01010122344";
+           $kunde1->navn = "Per Olsen";
+           $kunde1->adresse = "Osloveien 82 0270 Oslo";
+           $kunde1->telefonnr="12345678";
+           $alleKunder[]=$kunde1;
+           $kunde2 = new kunde();
+           $kunde2->personnummer ="01010122344";
+           $kunde2->navn = "Line Jensen";
+           $kunde2->adresse = "Askerveien 100, 1379 Asker";
+           $kunde2->telefonnr="92876789";
+           $alleKunder[]=$kunde2;
+           $kunde3 = new kunde();
+           $kunde3->personnummer ="02020233455";
+           $kunde3->navn = "Ole Olsen";
+           $kunde3->adresse = "Bærumsveien 23, 1234 Bærum";
+           $kunde3->telefonnr="99889988";
+           $alleKunder[]=$kunde3;
+           return $alleKunder;
+        }
+        
+        function registrerKunde($kunde){
+            
+        }
+        
+        function slettKunde($personnummer)
+        {
+            
+        }
+        
+        function registerKonto($konto)
+        {
+            
+        }
+        
+        function endreKonto($konto)
+        {
+            
+        }
+        
+        function hentAlleKonti()
+        {
+            
+        }
+        
+        function slettKonto($kontonummer)
+        {
+            
+        }
+    }
--- DAL/bankDatabaseStub.php
+++ DAL/bankDatabaseStub.php
@@ -1,39 +1,7 @@
 <?php
     include_once '../Model/domeneModell.php';
     class BankDBStub
-    {
-        function hentEnKunde($personnummer)
-        {
-           $enKunde = new kunde();
-           $enKunde->personnummer =$personnummer;
-           $enKunde->navn = "Per Olsen";
-           $enKunde->adresse = "Osloveien 82, 0270 Oslo";
-           $enKunde->telefonnr="12345678";
-           return $enKunde;
-        }
-        function hentAlleKunder()
-        {
-           $alleKunder = array();
-           $kunde1 = new kunde();
-           $kunde1->personnummer ="01010122344";
-           $kunde1->navn = "Per Olsen";
-           $kunde1->adresse = "Osloveien 82 0270 Oslo";
-           $kunde1->telefonnr="12345678";
-           $alleKunder[]=$kunde1;
-           $kunde2 = new kunde();
-           $kunde2->personnummer ="01010122344";
-           $kunde2->navn = "Line Jensen";
-           $kunde2->adresse = "Askerveien 100, 1379 Asker";
-           $kunde2->telefonnr="92876789";
-           $alleKunder[]=$kunde2;
-           $kunde3 = new kunde();
-           $kunde3->personnummer ="02020233455";
-           $kunde3->navn = "Ole Olsen";
-           $kunde3->adresse = "Bærumsveien 23, 1234 Bærum";
-           $kunde3->telefonnr="99889988";
-           $alleKunder[]=$kunde3;
-           return $alleKunder;
-        }
+    {        
         function hentTransaksjoner($kontoNr,$fraDato,$tilDato)
         {
             date_default_timezone_set("Europe/Oslo");
@@ -86,5 +54,60 @@
                 $dato +=(60*60*24); // en dag i tillegg i sekunder
             }
             return $konto;
+        }
+        
+        function sjekkLoggInn($personnummer,$passord)
+        {
+            
+        }
+        
+        function hentKonti($personnummer)
+        {
+            
+        }
+        
+        function hentSaldi($personnummer)
+        {
+            
+        }
+        
+        function registrerBetaling($kontoNr, $transaksjon)
+        {
+            
+        }
+        
+        function hentBetalinger($personnummer)
+        {
+            
+        }
+        
+        function utforBetaling($TxID)
+        {
+            
+        }
+        
+        function endreKundeInfo($kunde)
+        {
+            
+        }
+        
+        function registrerKunde($kunde)
+        {
+            
+        }
+        
+        function slettKunde($personnummer)
+        {
+            
+        }
+        
+        function hentKundeInfo($personnummer)
+        {
+           $enKunde = new kunde();
+           $enKunde->personnummer =$personnummer;
+           $enKunde->navn = "Per Olsen";
+           $enKunde->adresse = "Osloveien 82, 0270 Oslo";
+           $enKunde->telefonnr="12345678";
+           return $enKunde;
         }
     }
