f4a4113e305175e362e75996a39984d07d9eccd4
HildeSKlyve <hildeklyve@gmail.com>
2020-01-23 12:24:47 +0100

    update navnefeil

--- Tests/adminEndreKontoTest.php
+++ Tests/adminEndreKontoTest.php
@@ -3,7 +3,7 @@ include_once '../Model/domeneModell.php';
 //include_once '../DAL/databaseStub.php';
 include_once '../BLL/bankLogikk.php';
 
-class hentTransaksjonerTest extends PHPUnit\Framework\TestCase {
+class adminEndreKontoTest extends PHPUnit\Framework\TestCase {
     
     
     
