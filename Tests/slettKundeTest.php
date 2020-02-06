7936d71472ff6babf8ad97a56337ad258fe7b7ee
renkjet <47675663+renkjet@users.noreply.github.com>
2020-02-06 11:18:11 +0100

    SlettKundeTest OK

--- Tests/slettKundeTest.php
+++ Tests/slettKundeTest.php
@@ -7,5 +7,18 @@ include_once '../BLL/bankLogikk.php';
 //Renate
 
 class slettKundeTest extends PHPUnit\Framework\TestCase {
+
+    function test_slettKunde_OK(){
+        $bankLogikk = new Admin(new BankDBStub());
+        $personnummer = 1;
+        $OK = $bankLogikk->slettKunde($personnummer);
+        $this->assertEquals("OK", $OK);
+    }
+
+    function test_slettKunde_feil(){
+        $bankLogikk = new Admin(new BankDBStub());
+        $personnummer = 5;
+        $feil = $bankLogikk->slettKunde($personnummer);
+        $this->assertEquals("Feil", $feil);
+    }
     
-}
