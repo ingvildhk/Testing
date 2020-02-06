0cdff3ec6e46ac24d6be52794947560ba24d9ce5
renkjet <47675663+renkjet@users.noreply.github.com>
2020-02-06 11:19:09 +0100

    .

--- Tests/slettKundeTest.php
+++ Tests/slettKundeTest.php
@@ -6,19 +6,23 @@ include_once '../BLL/bankLogikk.php';
 //hentTransaksjonerTest: Laget av Tor, se for eksempel
 //Renate
 
-class slettKundeTest extends PHPUnit\Framework\TestCase {
+class slettKundeTest extends PHPUnit\Framework\TestCase
+{
 
-    function test_slettKunde_OK(){
+    function test_slettKunde_OK()
+    {
         $bankLogikk = new Admin(new BankDBStub());
         $personnummer = 1;
         $OK = $bankLogikk->slettKunde($personnummer);
         $this->assertEquals("OK", $OK);
     }
 
-    function test_slettKunde_feil(){
+    function test_slettKunde_feil()
+    {
         $bankLogikk = new Admin(new BankDBStub());
         $personnummer = 5;
         $feil = $bankLogikk->slettKunde($personnummer);
         $this->assertEquals("Feil", $feil);
     }
-    
+
+}
